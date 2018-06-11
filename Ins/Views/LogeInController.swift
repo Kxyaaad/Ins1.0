1360b1a97201d91db7cae0d1b8bfefcdb2c5374a
kxy <823942471@qq.com>
2018-06-11 21:52:47 +0800

    添加登录页面

---
+++ Ins/Views/LogeInController.swift
@@ -0,0 +1,44 @@
+//
+//  LogeInController.swift
+//  Ins
+//
+//  Created by Kxy on 2018/6/11.
+//  Copyright © 2018年 HUAMEi. All rights reserved.
+//
+
+import UIKit
+
+class LogeInController: UIViewController {
+
+    @IBOutlet weak var longIn: UIButton!
+    @IBOutlet weak var SingIn: UIButton!
+    @IBOutlet weak var UserName: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var FindPW: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func findPWact(_ sender: Any) {
+    }
+    
+    @IBAction func Logact(_ sender: Any) {
+    }
+    
+    @IBAction func SiginAct(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
