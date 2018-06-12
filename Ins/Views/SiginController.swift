c04b3eec9253168be8be85cdaa0f9ef252c2688f
kxy <823942471@qq.com>
2018-06-12 21:46:44 +0800

    添加键盘动画

--- Ins/Views/SiginController.swift
+++ Ins/Views/SiginController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import NotificationCenter
 
-class SiginController: UIViewController {
+class SiginController: UIViewController,UITextFieldDelegate {
     
     @IBOutlet weak var img: UIImageView!
     
@@ -24,15 +25,65 @@ class SiginController: UIViewController {
     
     @IBOutlet weak var scview: UIScrollView!
     
+    @IBOutlet weak var ok: UIButton!
+    @IBOutlet weak var cancel: UIButton!
     
+    var keyboardsize : CGRect!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//添加键盘通知
+        NotificationCenter.default.addObserver(self, selector: #selector(showKeyboard), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(hideKeyboarf), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        //键盘delegate绑定
+        self.username.delegate = self
+        self.password.delegate = self
+        self.password2th.delegate = self
+        self.email.delegate = self
+        self.name.delegate = self
+        self.jianjie.delegate = self
+        self.websit.delegate = self
+        
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.name {
+            self.username.resignFirstResponder()
+            self.password.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    
     override func viewDidAppear(_ animated: Bool) {
-        self.scview.contentSize = CGSize(width: self.view.frame.width, height: 500)
+        self.scview.contentSize = CGSize(width: self.view.frame.width, height: 800)
+    }
+    
+    @IBAction func zhcue(_ sender: Any) {
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func showKeyboard(notification: Notification) {
+        //定义键盘大小
+        let rect = notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        keyboardsize = rect as? CGRect
+        print(keyboardsize)
+        
+        //当虚拟键盘出现以后，将滚动师徒的实际高度所有为屏幕高度减去键盘的高度
+        UIView.animate(withDuration: 0.4) {
+            self.scview.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height:  (self.scview.frame.height - self.keyboardsize.height))
+        }
+    }
+    
+    @objc func hideKeyboarf(notification: Notification){
+        
+        UIView.animate(withDuration: 0.4) {
+            self.scview.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height:  self.view.frame.height)
+        }
+        
     }
     /*
     // MARK: - Navigation
