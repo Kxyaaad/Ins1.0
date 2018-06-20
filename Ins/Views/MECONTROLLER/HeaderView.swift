63d944140996bfb9d1833ec544fcc96222c943bf
kxy <823942471@qq.com>
2018-06-20 21:54:04 +0800

    添加了集合视图，并进行绑定

---
+++ Ins/Views/MECONTROLLER/HeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderView.swift
+//  Ins
+//
+//  Created by Kxy on 2018/6/20.
+//  Copyright © 2018年 HUAMEi. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UICollectionReusableView {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBOutlet weak var tiezi: UILabel!
+    
+    @IBOutlet weak var fensi: UILabel!
+    
+    @IBOutlet weak var guanzhu: UILabel!
+}
---
+++ Ins/Views/MECONTROLLER/PictureCell.swift
@@ -0,0 +1,14 @@
+//
+//  PictureCell.swift
+//  Ins
+//
+//  Created by Kxy on 2018/6/20.
+//  Copyright © 2018年 HUAMEi. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+    @IBOutlet weak var PicCell: UIImageView!
+    
+}
